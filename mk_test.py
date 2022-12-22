d1372ec560839f2a88ac1a29d08a837aaa9f52fd
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2022-12-23 00:15:23 +0900

    mk_test.py added

--- mk_test.py
+++ mk_test.py
@@ -1,20 +1,35 @@
-# 1873-2014 42models have responsible year->(42, 24, 72, 5), (42)
+# 1873-2014 42models have responsible year->(42, 24, 72, 4), (42)
 import numpy as np
 
+def main():
+    tors = 'predictors_coarse_std_Apr_msot'
+    tant = 'pr_1x1_std_MJJASO_one'
+    make_test(tors, tant)
+
 def load(inkey, outkey):
-    infile = f"/docker/mnt/d/research/D2/resnet/predictors/{inkey}.npy"
-    outfile = f"/docker/mnt/d/research/D2/resnet/predictant/{outkey}.npy"
+    infile = f"/work/kajiyama/cnn/input/predictors/predictors/{inkey}.npy"
+    outfile = f"/work/kajiyama/cnn/input/pr/one/1x1/{outkey}.npy"
     predictors = np.load(infile)
     predictant = np.load(outfile)
     return predictors, predictant
 
 def make_test(inkey, outkey):
-    savedir = f"/docker/mnt/d/research/D2/resnet/test"
+    savedir = f"/work/kajiyama/cnn/test"
     save_predictors = f"{savedir}/{inkey}_1973-2014.npy"
-    save_predictant = f"{savedir}/{outkey}_173-2014.npy"
+    save_predictant = f"{savedir}/{outkey}_1973-2014.npy"
     predictors, predictant = load(inkey, outkey)
     x_test, y_test, = np.empty((42, 24, 72, predictors. shape[-1])), np.empty(42)
     for i in range(42):
          x_test[i, :, :, :] = predictors[0+i, 172+i]
          y_test[i] = predictant[0+i, 172+i]
+    np.save(save_predictors, x_test)
+    np.save(save_predictant, y_test)
+
+if __name__ == '__main__':
+    try:
+        main()
+        print('[\N{check mark}]')
+    except Exception as e:
+        print('[\N{cross mark}]')
+        print(e)
 
