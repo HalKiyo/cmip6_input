d6b87f93bc31f566d866832272b095441b66922a
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-02-21 15:43:11 +0900

    y_test grid bug fixed

--- mk_test.py
+++ mk_test.py
@@ -2,12 +2,13 @@
 import numpy as np
 
 def main():
-    tors = 'predictors_coarse_std_Apr_o'
-    tant = 'pr_1x1_std_MJJASO_thailand'
     indir = '/predictors/coarse_std'
-    outdir = '/pr/continuous/thailand/1x1'
-    typ_flg = 'thialand'
-    make_test(tors, tant, indir, outdir, typ_flg=typ_flg)
+    tors = 'predictors_coarse_std_Apr_o'
+    outdir = '/pr/class/thailand/EFD'
+    tant = 'pr_1x1_std_MJJASO_thailand_EFD_5'
+    typ_flg = 'thailand'
+    grid = 20
+    make_test(tors, tant, indir, outdir, typ_flg=typ_flg, grid=grid)
 
 def load(inkey, outkey, indir, outdir):
     workdir = '/work/kajiyama/cnn/input'
@@ -17,20 +18,19 @@ def load(inkey, outkey, indir, outdir):
     predictant = np.load(outfile)
     return predictors, predictant
 
-def make_test(inkey, outkey, indir, outdir, typ_flg='continuous'):
+def make_test(inkey, outkey, indir, outdir, typ_flg='continuous', grid=20):
     savedir = f"/work/kajiyama/cnn/test"
     save_predictors = f"{savedir}/{inkey}_1973-2014.npy"
     save_predictant = f"{savedir}/{outkey}_1973-2014.npy"
     predictors, predictant = load(inkey, outkey, indir, outdir)
-    x_test, y_test, = np.empty((predictors.shape[0], 42, 24, 72)), np.empty(42)
+    x_test = np.empty((predictors.shape[0], 42, 24, 72))
+    y_test = np.empty(42)
     if typ_flg == 'one':
         for i in range(42):
-            print(predictors.shape)
-            print(predictant.shape)
-            exit()
             x_test[:, i, :, :] = predictors[:, 0+i, (165-42)+i]
             y_test[i] = predictant[0+i, (165-42)+i]
     elif typ_flg == 'thailand':
+        y_test = np.empty((42, grid, grid))
         for i in range(42):
             x_test[:, i, :, :] = predictors[:, 0+i, (165-42)+i]
             y_test[i] = predictant[0+i, (165-42)+i, :, :]
