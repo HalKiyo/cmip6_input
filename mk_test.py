9e518ba629f8cd8abbcc9fcc9fddc1f617842273
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-02-22 17:30:23 +0900

    train for selected test_year

--- mk_test.py
+++ mk_test.py
@@ -4,11 +4,11 @@ import numpy as np
 def main():
     indir = '/predictors/coarse_std'
     tors = 'predictors_coarse_std_Apr_o'
-    outdir = '/pr/class/thailand/EFD'
-    tant = 'pr_1x1_std_MJJASO_thailand_EFD_5'
+    outdir = '/pr/continuous/thailand/1x1'
+    tant = 'pr_1x1_std_MJJASO_thailand'
     typ_flg = 'thailand'
     grid = 20
-    make_test(tors, tant, indir, outdir, typ_flg=typ_flg, grid=grid)
+    test_selection(tors, tant, indir, outdir, typ_flg=typ_flg, grid=grid)
 
 def load(inkey, outkey, indir, outdir):
     workdir = '/work/kajiyama/cnn/input'
@@ -18,30 +18,58 @@ def load(inkey, outkey, indir, outdir):
     predictant = np.squeeze(np.load(outfile))
     return predictors, predictant
 
-def make_test(inkey, outkey, indir, outdir, typ_flg='continuous', grid=20):
+def test_selection(inkey, outkey, indir, outdir, typ_flg='continuous', grid=20):
     savedir = f"/work/kajiyama/cnn/test"
-    save_predictors = f"{savedir}/{inkey}_1973-2014.npy"
-    save_predictant = f"{savedir}/{outkey}_1973-2014.npy"
+    save_predictors = f"{savedir}/val/val_{inkey}_1973-2014.npy"
+    save_predictant = f"{savedir}/val/val_{outkey}_1973-2014.npy"
+    train_predictors = f"{savedir}/train/train_{inkey}_1973-2014.npy"
+    train_predictant = f"{savedir}/train/train_{outkey}_1973-2014.npy"
+
     predictors, predictant = load(inkey, outkey, indir, outdir)
+
+    x_train = np.empty((42*165 - 42, 24, 72))
+    y_train = np.empty(165*42 - 42)
+
     x_test = np.empty((42, 24, 72))
     y_test = np.empty(42)
+
     if typ_flg == 'one':
         print('one')
+        index = 0
         for i in range(42):
-            x_test[i, :, :] = predictors[0+i, (165-42)+i]
-            y_test[i] = predictant[0+i, (165-42)+i]
+            for j in range(165):
+                if i == (j-(165-42)):
+                    x_test[i, :, :] = predictors[i, j]
+                    y_test[i] = predictant[i, j]
+                else:
+                    x_train[index, :, :] = predictors[i, j]
+                    y_train[index] = predictant[i, j]
+                    index += 1
+                    print(index)
+
     elif typ_flg == 'thailand':
         print('thailand')
+        index = 0
+        y_train = np.empty((165*42 - 42, grid, grid))
         y_test = np.empty((42, grid, grid))
         for i in range(42):
-            x_test[i, :, :] = predictors[0+i, (165-42)+i]
-            print(predictors[0+i, (165-42)+i])
-            exit()
-            y_test[i, :, :] = predictant[0+i, (165-42)+i, :, :]
+            for j in range(165):
+                if i == (j-(165-42)):
+                    x_test[i, :, :] = predictors[i, j]
+                    y_test[i, :, :] = predictant[i, j, :, :]
+                else:
+                    x_train[index, :, :] = predictors[i, j]
+                    y_train[index, :, :] = predictant[i, j, :, :]
+                    index += 1
+                    print(index)
+
+    np.save(train_predictors, np.squeeze(x_train))
+    np.save(train_predictant, y_train)
+
     np.save(save_predictors, np.squeeze(x_test))
     np.save(save_predictant, y_test)
 
-def make_train():
+def test_random():
     pass
 
 if __name__ == '__main__':
