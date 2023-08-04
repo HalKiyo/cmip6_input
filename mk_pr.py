ba488b96140065e9e161704e6a468c1c2d9c0bad
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-08-04 19:21:30 +0900

    monsoon added

--- mk_pr.py
+++ mk_pr.py
@@ -37,14 +37,17 @@ def pr_1x1():
     workdir = '/work/kajiyama/cnn/input/pr'
 
     resolution = '1x1'
-    lat_lst = [7*5, 11*5]
-    lon_lst = [18*5, 22*5]
+    lat_lst = [7*5, 11*5] # 5-25N
+    lon_lst = [18*5, 22*5] # 90-110E
+    ma_lat = [1*5, 15*5] # 15S-55N
+    ma_lon = [12*5, 30*5] # 60-150E
     form_lst = ['raw', 'anom', 'std']
     msk = 20
 
     variable = 'pr'
     lead_lst = [1, 3, 6]
-    space_lst = ['one', 'thailand', 'world']
+    space_lst = ['monsoon']
+    #space_lst = ['one', 'thailand', 'world']
     model_num = 42
     year_num = 165
 
@@ -66,6 +69,10 @@ def pr_1x1():
                         thailand = loaded[:, :, mon_arr[i,0]:mon_arr[i,1], lat_lst[0]:lat_lst[1], lon_lst[0]:lon_lst[1]]
                         out = np.mean(thailand, axis=2)
 
+                    elif space == 'monsoon':
+                        monsoon = loaded[:, :, mon_arr[i,0]:mon_arr[i,1],  ma_lat[0]:ma_lat[1], ma_lon[0]:ma_lon[:]]
+                        out = np.mean(monsoon, axis=2)
+
                     elif space == 'world':
                         world = loaded[:, :, mon_arr[i,0]:mon_arr[i,1], :, :]
                         out = np.mean(world, axis=2)
@@ -78,14 +85,17 @@ def pr_5x5():
     workdir = '/work/kajiyama/cnn/input/pr'
 
     resolution = '5x5'
-    lat_lst = [7, 11]
-    lon_lst = [18, 22]
+    lat_lst = [7, 11] # 5-25N
+    lon_lst = [18, 22] # 90-110E
+    ma_lat = [1, 15] # 15S-55N
+    ma_lon = [12, 30] # 60-150E
     form_lst = ['coarse', 'coarse_anom', 'coarse_std']
     msk = 4
 
     variable = 'pr'
     lead_lst = [1, 3, 6]
-    space_lst = ['one', 'thailand', 'world']
+    space_lst = ['monsoon']
+    #space_lst = ['one', 'thailand', 'world']
     model_num = 42
     year_num = 165
 
@@ -105,6 +115,10 @@ def pr_5x5():
                     elif space == 'thailand':
                         thailand = loaded[:, :, mon_arr[i,0]:mon_arr[i,1], lat_lst[0]:lat_lst[1], lon_lst[0]:lon_lst[1]]
                         out = np.mean(thailand, axis=2)
+
+                    elif space == 'monsoon':
+                        monsoon = loaded[:, :, mon_arr[i,0]:mon_arr[i,1],  ma_lat[0]:ma_lat[1], ma_lon[0]:ma_lon[:]]
+                        out = np.mean(monsoon, axis=2)
 
                     elif space == 'world':
                         world = loaded[:, :, mon_arr[i,0]:mon_arr[i,1], :, :]
